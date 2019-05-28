4705f35c97a028304db224f5c8d18c9f4501197c
Cristiano Breuel <cristiano.breuel@gmail.com>
2019-05-28 13:36:36 -0300

    Add .bash_aliases and git clone.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+git clone git@github.com:cbreuel/devenv.git
+cd devenv
+
+cp .bash_aliases ~/.bash_aliases
+
 # Install tmux and tmx
 sudo apt-get install tmux
 curl -L https://raw.githubusercontent.com/arkku/dotfiles/master/bin/tmx -o ~/bin/tmx
