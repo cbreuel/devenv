e59807279760c3591167a8c76a0b4e1673454ef9
Cristiano Breuel <cristiano.breuel@gmail.com>
2020-03-11 20:27:54 -0300

    Add Spark

--- setup.sh
+++ setup.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+# Move to temp dir
+WORKDIR=$(mktemp -d)
+cd $WORKDIR
+
+# Install git and clone repo
 sudo apt-get install -y --no-install-recommends git
 git clone https://github.com/cbreuel/devenv.git
 cd devenv
 
+# Overwrite .bash_aliases
 cp -f .bash_aliases ~/.bash_aliases
 
 # Install tmux and tmx
 sudo apt-get install -y --no-install-recommends tmux
-curl -L https://raw.githubusercontent.com/arkku/dotfiles/master/bin/tmx -o ~/bin/tmx
+curl -L https://raw.githubusercontent.com/arkku/dotfiles/master/bin/tmx -o ./tmx
+cp -f ./tmx ~/bin/tmx
 chmod u+x ~/bin/tmx
 cp -f ./.tmux.conf ~/.tmux.conf
 
